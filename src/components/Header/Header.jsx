4228ef7eb23e8f7c5cf946871f0228bcf2366faa
igerne94 <igerne@gmail.com>
2024-09-05 16:38:04 +0200

    compared w useCallback hook

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,10 +6,18 @@ import Logo from '../Logo/Logo';
 const logos = ['/logo.svg', '/vite.svg'];
 
 const Header = () => {
+    console.log('Header rendered');
     const [logoIndex, setLogoIndex] = useState(0);
     const loggleLogo = () => {
+        // the result of the callback function should be memoized
+        //...if no params change.
+        //...And the function will not be recreated
         setLogoIndex(logoIndex => Number(!logoIndex));
     };
+
+    // const loggleLogo = useCallback(() => {
+    //     setLogoIndex(logoIndex => Number(!logoIndex));
+    // }, [logoIndex]);
     
     return (
         <>
