00079db92594d48b038d3ef92288fdf774bc5cb9
igerne94 <igerne@gmail.com>
2024-09-06 17:50:45 +0200

    JournalList useMemo() hook

--- src/components/JournalList/JournalList.jsx
+++ src/components/JournalList/JournalList.jsx
@@ -1,26 +1,28 @@
 import './JournalList.css';
 import CardButton from '../CardButton/CardButton';
 import JournalItem from '../JournalItem/JournalItem';
-import { useContext } from 'react';
+import { useContext, useMemo } from 'react';
 import { UserContext } from '../../context/UserContext';
 
 // Presentational component
 function JournalList({ items }) {
     const { userId } = useContext(UserContext);
-
-    if (items.length === 0) {
-        return <p>No entry found</p>
-    }
-
     const sortItems = (a, b) => {
         if (a.date < b.date) return 1;
         else return -1;
     };
 
-    return <>
-        {items
+    const filteredAndSortedItems =
+        useMemo(() => items
             .filter(elem => elem.userId === userId)
-            .sort(sortItems)
+            .sort(sortItems), [items, userId]);
+    
+    if (items.length === 0) {
+        return <p>No entry found</p>
+    }
+
+    return <>
+        {filteredAndSortedItems
             .map((item) => {
                 return (
                     <CardButton key={item.id} >
